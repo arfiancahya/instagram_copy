38a292f3133886df7db3e2a3309ca96f8c13e439
arfiancahya <arfian.dwisetya@gmail.com>
2020-09-20 12:43:53 +0700

    Membernarkan Login

---
+++ static/js/login.js
@@ -0,0 +1,29 @@
+function onSingIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log("Full Name: " + profile.getName());
+    console.log("Email: " + profile.getEmail());
+
+    const x = document.getElementsByClassName("logged");
+    for(let i = 0; i <= x.length; i++) {
+        x[0].classList.remove("logged");
+    }
+}
+
+function onSingOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.singOut().then(function () {
+        console.log('User Signed Out.');
+        hideDashboard();
+    });
+}
+
+function hideDashboard() {
+    const x = document.getElementsByClassName('onlylogged');
+    for(let i = 0; i <= x.length; i++) {
+        x[i].classList.add("logged");
+    }
+}
+
+function gotoDashboard() {
+    window.location.replace("http://localhost:9000/index.html");
+}
