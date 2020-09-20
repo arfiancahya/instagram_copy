13bd8712221ca6452d2ae9e1ec01871bcb2659a3
arfiancahya <arfian.dwisetya@gmail.com>
2020-09-20 12:57:10 +0700

    Membernarkan Login

--- static/js/login.js
+++ static/js/login.js
@@ -9,9 +9,9 @@ function onSingIn(googleUser) {
     }
 }
 
-function onSingOut() {
+function onSignOut() {
     const auth2 = gapi.auth2.getAuthInstance();
-    auth2.singOut().then(function () {
+    auth2.signOut().then(function () {
         console.log('User Signed Out.');
         hideDashboard();
     });
